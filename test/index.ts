bc67546a7b7fb342286c73fca30e34985bbbac0e
Dion Shi <dion.shi.hk@gmail.com>
2020-06-15 16:10:32 +0800

    Delete index.ts

--- test/index.ts
+++
@@ -1,8 +0,0 @@
-import {startAppCenterBuild} from "../src";
-import {config} from "./config";
-
-/**
- * Attention:
- * Real configuration is git ignored, do not push to repo
- */
-startAppCenterBuild(config);
