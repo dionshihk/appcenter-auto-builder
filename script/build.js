3dacb9e04de74ac58474a9eaef5924881a5093ac
dionshi <dion.shi.hk@gmail.com>
2021-07-12 17:50:16 +0800

    Upgrade deps and simplify build script.

--- script/build.js
+++ script/build.js
@@ -1,8 +1,7 @@
-const chalk = require("chalk");
 const childProcess = require("child_process");
 const fs = require("fs-extra");
 
-function spawn(command, args, errorMessage) {
+function spawn(command, args) {
     const isWindows = process.platform === "win32"; // spawn with {shell: true} can solve .cmd resolving, but prettier doesn't run correctly on mac/linux
     const result = childProcess.spawnSync(isWindows ? command + ".cmd" : command, args, {stdio: "inherit"});
     if (result.error) {
@@ -10,29 +9,28 @@ function spawn(command, args, errorMessage) {
         process.exit(1);
     }
     if (result.status !== 0) {
-        console.error(chalk`{red.bold ${errorMessage}}`);
         console.error(`non-zero exit code returned, code=${result.status}, command=${command} ${args.join(" ")}`);
         process.exit(1);
     }
 }
 
 function cleanup() {
-    console.info(chalk`{green.bold [task]} {white.bold cleanup}`);
+    console.info("Cleaning up ...");
     fs.emptyDirSync("build");
 }
 
 function checkCodeStyle() {
-    console.info(chalk`{green.bold [task]} {white.bold check code style}`);
-    return spawn("prettier", ["--config", "./prettier.config.js", "--list-different", "{src,test}/**/*.ts"], "check code style failed, please format above files");
+    console.info("Checking code style ...");
+    return spawn("prettier", ["--config", "./prettier.config.js", "--list-different", "{src,test}/**/*.ts"]);
 }
 
 function compile() {
-    console.info(chalk`{green.bold [task]} {white.bold compile}`);
-    return spawn("tsc", ["-p", "./tsconfig.json"], "compile failed, please fix");
+    console.info("Compiling TypeScript ...");
+    return spawn("tsc", ["-p", "./tsconfig.json"]);
 }
 
 function distribute() {
-    console.info(chalk`{green.bold [task]} {white.bold distribute}`);
+    console.info("Distributing ...");
     fs.mkdirsSync("build/dist");
     fs.copySync("build/out/src", "build/dist", {dereference: true});
     fs.copySync("package.json", "build/dist/package.json", {dereference: true});
@@ -46,8 +44,7 @@ function build() {
     compile();
     distribute();
 
-    console.info(chalk`{yellow.bold Build Successfully}`);
-    console.info(chalk`{yellow.bold You can publish to npm via:} npm publish build/dist/`);
+    console.info("Build done, you can publish to npm via `npm publish build/dist/`");
 }
 
 build();
